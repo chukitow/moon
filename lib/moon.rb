bc9b90159f01461679cc33c409d15741ea81563d
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-10-18 15:59:38 -0500

    add and configure dependencies

--- lib/moon.rb
+++ lib/moon.rb
@@ -1,4 +1,10 @@
 require "moon/engine"
+require 'bootstrap-sass'
+require 'sass-rails'
+require 'haml-rails'
+require 'jquery-rails'
+require 'bootstrap_form'
+require 'bootstrap-datepicker-rails'
 
 module Moon
 end
