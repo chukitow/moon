4395f5ff61a12b9a6d0f88d45bfa53609dc6e781
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2016-10-19 17:55:16 -0500

    every product should have a master variant

--- app/models/moon/product.rb
+++ app/models/moon/product.rb
@@ -2,9 +2,14 @@ class Moon::Product < ActiveRecord::Base
   has_many :images, as: :viewable, class_name: 'Moon::Asset'
   has_many :product_option_types
   has_many :option_types, through: :product_option_types
-
-  validates_presence_of :name, :master_price
-  validates_numericality_of :master_price, only_integer: true, greater_than_or_equal_to: 0
+  has_many :variants, dependent: :destroy
 
   accepts_nested_attributes_for :option_types, allow_destroy: true
+  accepts_nested_attributes_for :variants, reject_if: :all_blank
+
+  validates_presence_of :name
+
+  def master_variant
+    variants.find_by(is_master: true)
+  end
 end
--- spec/models/moon/product_spec.rb
+++ spec/models/moon/product_spec.rb
@@ -2,6 +2,4 @@ require 'spec_helper'
 
 describe Moon::Product do
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:master_price) }
-  it { should validate_numericality_of(:master_price) }
 end
